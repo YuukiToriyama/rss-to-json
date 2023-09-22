7c2bbcc15543135a841fbeca38b8f5369333f49c
Yuuki Toriyama <github@toriyama.dev>
2023-09-23 00:24:32 +0900

    <title>タグを読めるようにした

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,15 @@ use hard_xml::XmlRead;
 struct Feed<'a> {
     #[xml(attr = "xmlns")]
     xmlns: Cow<'a, str>,
+    #[xml(child = "title")]
+    title: Option<Title<'a>>,
+}
+
+#[derive(XmlRead, PartialEq, Debug)]
+#[xml(tag = "title")]
+struct Title<'a> {
+    #[xml(text)]
+    text: Cow<'a, str>,
 }
 
 #[cfg(test)]
@@ -16,6 +25,15 @@ mod tests {
     fn parse_simple_tag() {
         let xml = r#"<feed xmlns="http://www.w3.org/2005/Atom"></feed>"#;
         let parsed_xml = Feed::from_str(xml).unwrap();
-        assert_eq!(parsed_xml, Feed { xmlns: "http://www.w3.org/2005/Atom".into() })
+        assert_eq!(parsed_xml, Feed { xmlns: "http://www.w3.org/2005/Atom".into(), title: None })
+    }
+
+    #[test]
+    fn parse_complex_xml() {
+        let xml = r#"<feed xmlns="http://www.w3.org/2005/Atom">
+            <title>Hogehoge RSS Feed</title>
+        </feed>"#;
+        let parsed_xml = Feed::from_str(xml).unwrap();
+        assert_eq!(parsed_xml, Feed { xmlns: "http://www.w3.org/2005/Atom".into(), title: Some(Title { text: "Hogehoge RSS Feed".into() }) })
     }
 }
